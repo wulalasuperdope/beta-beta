3d3f2273a3a1fd46f68440a40b54270c346f1d83
Cissy Bai <cissy5120@gmail.com>
2020-02-12 17:52:59 -0500

    updated root.js

--- app/components/root.js
+++ app/components/root.js
@@ -1,7 +1,41 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom';
 
-const Root = () => {
-  return <div>hello beta</div>;
-};
+class Root extends React.Component {
+  componentDidMount() {}
 
-export { Root };
+  render() {
+    return (
+      <Router>
+        <div>
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/routes">Routes</Link>
+          </nav>
+          <main>
+            <h1>Welcome to the Beta Beta</h1>
+            <h3>
+              Having a problem on sending your project? Check out some beta for
+              the same route.
+            </h3>
+          </main>
+          <Switch>
+            <Route path="/routes" component={AllRoutes} />
+            <Route path="/routes/:routeId" component={SingleRoute} />
+            <Redirect to="/" />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
+
+const mapState = state => state;
+
+export default connect(mapState)(Root);
