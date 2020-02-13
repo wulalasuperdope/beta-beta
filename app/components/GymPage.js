c42bb84bcd0b6d12acc19345df3f38748d06fcba
Cissy Bai <cissy5120@gmail.com>
2020-02-12 23:58:52 -0500

    fixing mounting issue in GymPage component

--- app/components/GymPage.js
+++ app/components/GymPage.js
@@ -5,24 +5,34 @@ import { fetchGym } from '../redux/thunks/GymThunks';
 
 class GymPage extends React.Component {
   componentDidMount() {
-    this.props.fetchGym(this.props.match.params.id);
+    console.log('calling componentDidMount in GymPage');
+    this.props.fetchGym(this.props.match.params.gymId);
   }
   render() {
     console.log('calling Gym Page render');
     console.log(this.props.singleGym);
     const { name, mapUrl, routes } = this.props.singleGym;
-
-    return (
-      <div className="single-gym-page">
-        <h3 className="gym-name">{name}</h3>
-        <img className="gym-map" src={mapUrl} />
-        <ListGroup className="routes-list">
-          {routes.map(route => (
-            <ListGroup.Item key={route.id}>{route.grade}</ListGroup.Item>
-          ))}
-        </ListGroup>
-      </div>
-    );
+    if (name && !routes.length) {
+      return (
+        <div className="single-gym-page">
+          <h3 className="gym-name">{name}</h3>
+          <img className="gym-map" src={mapUrl} />
+          <p>This gym doesn't update any route here.</p>
+        </div>
+      );
+    } else {
+      return (
+        <div className="single-gym-page">
+          <h3 className="gym-name">{name}</h3>
+          <img className="gym-map" src={mapUrl} />
+          <ListGroup className="routes-list">
+            {routes.map(route => (
+              <ListGroup.Item key={route.id}>{route.grade}</ListGroup.Item>
+            ))}
+          </ListGroup>
+        </div>
+      );
+    }
   }
 }
 
